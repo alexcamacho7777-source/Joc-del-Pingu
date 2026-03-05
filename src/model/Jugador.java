a87bf6343813e927aebda375bee9800a412e9cad
Alex Camacho <alexcamacho7777@alumnes.ilerna.com>
2026-03-05 15:32:09 +0100

    Clase Jugador

--- src/model/Jugador.java
+++ src/model/Jugador.java
@@ -1,7 +1,42 @@
 package model;
 
-public class Jugador {
+public abstract class Jugador {
 
-	
-	
+    protected int posicion;
+    protected String nombre;
+    protected String color;
+
+    public Jugador(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+        this.posicion = 0;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public String getNombre() {
+    	return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void moverPosicion(int p) {
+        this.posicion += p;
+    }
 }
