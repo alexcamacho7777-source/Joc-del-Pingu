a743c830ef759b92816963b293970ea1972f701c
alexmla696-del <alex.m.la696@alumnes.ilerna.com>
2026-03-09 15:32:30 +0100

    classe pinguino

--- src/model/Pinguino.java
+++ src/model/Pinguino.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Pinguino {
+public class Pinguino extends Jugador {
 
+    private Inventario inv;
+
+    public Pinguino(String nombre, String color) {
+        super(nombre, color);
+        this.inv = new Inventario();
+    }
+
+    public Inventario getInv() {
+        return inv;
+    }
+
+    public void setInv(Inventario inv) {
+        this.inv = inv;
+    }
+
+    public void gestionarBatalla(Pinguino p) {
+        System.out.println(getNombre() + " lucha contra " + p.getNombre());
+    }
+
+    public void usarItem(Item i) {
+        System.out.println(getNombre() + " usa el item " + i.getNombre());
+    }
+
+    public void anadirItem(Item i) {
+        inv.anadirItem(i);
+    }
+
+    public void quitarItem(Item i) {
+        inv.quitarItem(i);
+    }
 }
