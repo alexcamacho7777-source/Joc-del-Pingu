84d6c5275828df4dcec0306edc7b3408a3f5402c
alexmla696-del <alex.m.la696@alumnes.ilerna.com>
2026-03-11 16:13:35 +0100

    paretida
    dsa

---
+++ src/model/Partida.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Partida {
+
+	//atributos
+    private Tablero tablero;
+    private ArrayList<Jugador> jugadores;
+    private int turnos;
+    private int jugadorActual;
+    private boolean finalizada;
+    private Jugador ganador;
+
+    public Partida(Tablero tablero, ArrayList<Jugador> jugadores) {
+        this.tablero = tablero;
+        this.jugadores = jugadores;
+        this.turnos = 0;
+        this.jugadorActual = 0;
+        this.finalizada = false;
+        this.ganador = null;
+    }
+
+    public Tablero getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(Tablero tablero) {
+        this.tablero = tablero;
+    }
+
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugador> jugadores) {
+        this.jugadores = jugadores;
+        
+    }
+
+    public int getTurnos() {
+        return turnos;
+    }
+
+    public void setTurnos(int turnos) {
+        this.turnos = turnos;
+    }
+
+    public int getJugadorActualIndex() {
+        return jugadorActual;
+    }
+
+    public void setJugadorActualIndex(int jugadorActual) {
+        this.jugadorActual = jugadorActual;
+    }
+
+    public boolean isFinalizada() {
+        return finalizada;
+    }
+
+    public void setFinalizada(boolean finalizada) {
+        this.finalizada = finalizada;
+    }
+
+    public Jugador getGanador() {
+        return ganador;
+    }
+
+    public void setGanador(Jugador ganador) {
+        this.ganador = ganador;
+    }
+
+    public Jugador getJugadorActual() {
+        return jugadores.get(jugadorActual);
+    }
+}
