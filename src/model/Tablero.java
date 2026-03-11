cff673bdf4f6ce581a5caa2e07cd38142708c42d
alexmla696-del <alex.m.la696@alumnes.ilerna.com>
2026-03-11 15:49:16 +0100

    commit
    classe casella partida i tablero

--- src/model/Tablero.java
+++ src/model/Tablero.java
@@ -1,5 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Tablero {
 
+    private ArrayList<Casilla> casillas;
+
+    public Tablero() {
+        casillas = new ArrayList<>();
+    }
+
+    public ArrayList<Casilla> getCasillas() {
+        return casillas;
+    }
+
+    public void setCasillas(ArrayList<Casilla> casillas) {
+        this.casillas = casillas;
+    }
+
+    public void actualizarTablero() {
+        // aquí se actualizaría el estado del tablero
+        System.out.println("Tablero actualizado");
+    }
 }
--- src/model/caselles/casella.java
+++
@@ -1,8 +0,0 @@
-package model.caselles;
-
-public class casella {
-
-
-	
-	
-}
