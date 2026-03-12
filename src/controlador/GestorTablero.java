d58fd7be6db4536c263313f63f17fa47df9b3060
Alex Camacho <alexcamacho7777@alumnes.ilerna.com>
2026-03-12 15:48:56 +0100

    Gestor Tablero

--- src/controlador/GestorTablero.java
+++ src/controlador/GestorTablero.java
@@ -1,5 +1,24 @@
 package controlador;
 
+import model.*;
+
 public class GestorTablero {
+
+    public void ejecutarCasilla(Partida partida, Pinguino p, Casilla c){
+
+        c.realizarAccion(partida, p);
+
+    }
+
+    public void comprobarFinTurno(Partida partida){
+
+        if(partida.getJugadorActual().getPosicion() >= 
+           partida.getTablero().getCasillas().size() - 1){
+
+            System.out.println("Un jugador ha llegado al final");
+
+        }
+
+    }
 
 }
